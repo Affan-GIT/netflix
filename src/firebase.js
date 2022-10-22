b3d2da9c73ffb78a95ff40de87359b3c3d079386
Affan-GIT <affanhamid007@gmail.com>
2022-10-22 21:53:34 +0530

    Updating project app

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCImREfGV2PQj1b5E4XKBtTlJrKbKFYMR4",
-  authDomain: "netflix-b27bc.firebaseapp.com",
-  projectId: "netflix-b27bc",
-  storageBucket: "netflix-b27bc.appspot.com",
-  messagingSenderId: "608005115985",
-  appId: "1:608005115985:web:bbd70e5d0d3c9164392870",
+  apiKey: "AIzaSyAh7J2i_MHgcFv2tpd1ulVZbn8RmbEwBds",
+  authDomain: "netflix-clone-da012.firebaseapp.com",
+  projectId: "netflix-clone-da012",
+  storageBucket: "netflix-clone-da012.appspot.com",
+  messagingSenderId: "631849569809",
+  appId: "1:631849569809:web:fb4c5ccf720be2243dabb0",
 };
