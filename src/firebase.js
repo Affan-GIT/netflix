94023c09cc05e185234a7b068c297c980014c539
Affan-GIT <affanhamid007@gmail.com>
2022-10-22 21:24:38 +0530

    Added Frontend

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCImREfGV2PQj1b5E4XKBtTlJrKbKFYMR4",
+  authDomain: "netflix-b27bc.firebaseapp.com",
+  projectId: "netflix-b27bc",
+  storageBucket: "netflix-b27bc.appspot.com",
+  messagingSenderId: "608005115985",
+  appId: "1:608005115985:web:bbd70e5d0d3c9164392870",
+};
